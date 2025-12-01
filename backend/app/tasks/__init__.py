98f7d4be13598c49ab6c05bda2402328729c5fb2
The Reviseit <thereviseit@gmail.com>
2025-12-01 13:17:03 +0530

    Delete backend directory

--- backend/app/tasks/__init__.py
+++
@@ -1,3 +0,0 @@
-# Celery tasks package
-from app.tasks.whatsapp_tasks import send_whatsapp_message, process_ai_response
-from app.tasks.campaign_tasks import execute_campaign, check_and_schedule_followups
